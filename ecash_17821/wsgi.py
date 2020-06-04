a62c98d561e44aaf9189191cf69b1f42e842a15f
Crowdbotics <team@crowdbotics.com>
2020-06-04 22:47:57 +0000

    Update models

--- ecash_17821/wsgi.py
+++ ecash_17821/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecash_17821.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecash_17821.settings")
 
 application = get_wsgi_application()
